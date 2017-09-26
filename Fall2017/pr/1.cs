290bf3a2b2c6b95a371876251e0086c91a45f0f7
SharipovShavkat11-709 <31937258+SharipovShavkat11-709@users.noreply.github.com>
2017-09-26 13:33:07 +0300

    add tasks

---
+++ Fall2017/pr/1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if ((Math.Pow(x, 2)) - Math.Pow(y, 2) >= 0) 
+            Console.WriteLine("yes"); 
+            else Console.WriteLine("no");
+            Console.ReadKey();
+        }
+    }
+}
