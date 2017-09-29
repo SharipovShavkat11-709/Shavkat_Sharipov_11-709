219ce8c6597f0d3078efa21fb0a5b32e8e3b7cef
SharipovShavkat11-709 <zaza_s07@mail.ru>
2017-09-29 08:59:57 +0300

    Add task of 25/09

---
+++ Fall2017/pr/0.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace _2_я_задача_25._09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 17,10,45 };
+            var Max = arr[0];
+            var i = 0;
+            
+            for (i = 0; i < arr.Length; i++) 
+
+            {
+                if (Max < arr[i])
+
+                {
+                    Max = arr[i];
+                    
+                    
+                }
+            }
+            Console.WriteLine(Max);
+
+        }
+    }
+}
---
+++ Fall2017/pr/2/2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 7, 15, 22, 10, 9 };
+            var max = arr[0];
+            var i = 0;
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }                    
+            }
+
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }        
+    }
+}
+
