91e3c1c6bc5755e972bf751a3ad76701a49e36eb
SharipovShavkat11-709 <zaza_s07@mail.ru>
2017-10-05 15:55:13 +0300

    добавил решение

---
+++ Fall2017/sem/1/1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            if(Convert.ToInt32(str[0]) + Convert.ToInt32(str[2]) + Convert.ToInt32(str[4]) == Convert.ToInt32(str[1]) + Convert.ToInt32(str[3]) + Convert.ToInt32(str[5]))
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
---
+++ Fall2017/sem/2/2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите вещественное число:");
+            double n = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите степень, в которую хотите возвести число:");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            double b = Math.Pow(n, k);
+            Console.WriteLine("Ответ:"+ b);
+            Console.ReadKey();
+        }
+    }
+}
